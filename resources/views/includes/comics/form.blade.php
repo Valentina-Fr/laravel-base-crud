74a2d6e5f8924a393cd87c8cb6244058080008c8
Valentina-Fr <v_frasca@virgilio.it>
2021-10-15 18:00:59 +0200

    Style form fields errors

--- resources/views/includes/comics/form.blade.php
+++ resources/views/includes/comics/form.blade.php
@@ -16,31 +16,38 @@
     @csrf
   <div class="mb-3">
     <label for="title" class="form-label">Title</label>
-    <input type="text" class="form-control" id="title" name="title" value="{{ old('title', $comic->title) }}">
+    <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" value="{{ old('title', $comic->title) }}">
+    @error('title')<div class="invalid-feedback">{{ $message }}</div>@enderror
   </div>
   <div class="mb-3">
     <label for="description" class="form-label">Description</label>
-    <textarea rows="5" class="form-control" id="description" name="description">{{ old('description', $comic->description) }}</textarea>
+    <textarea rows="5" class="form-control @error('description') is-invalid @enderror" id="description" name="description">{{ old('description', $comic->description) }}</textarea>
+    @error('description')<div class="invalid-feedback">{{ $message }}</div>@enderror
   </div>
   <div class="mb-3">
     <label for="thumb" class="form-label">Thumb</label>
-    <input type="text" class="form-control" id="thumb" name="thumb" value="{{ old('thumb', $comic->thumb) }}">
+    <input type="text" class="form-control @error('thumb') is-invalid @enderror" id="thumb" name="thumb" value="{{ old('thumb', $comic->thumb) }}">
+    @error('thumb')<div class="invalid-feedback">{{ $message }}</div>@enderror
   </div>
   <div class="mb-3">
     <label for="price" class="form-label">Price</label>
-    <input type="number" class="form-control" id="price" step=".01" name="price" value="{{ old('price', $comic->price) }}">
+    <input type="number" class="form-control @error('price') is-invalid @enderror" id="price" step=".01" name="price" value="{{ old('price', $comic->price) }}">
+    @error('price')<div class="invalid-feedback">{{ $message }}</div>@enderror
   </div>
   <div class="mb-3">
     <label for="series" class="form-label">Series</label>
-    <input type="text" class="form-control" id="series" name="series" value="{{ old('series', $comic->series) }}">
+    <input type="text" class="form-control @error('series') is-invalid @enderror" id="series" name="series" value="{{ old('series', $comic->series) }}">
+    @error('series')<div class="invalid-feedback">{{ $message }}</div>@enderror
   </div>
   <div class="mb-3">
     <label for="sale-date" class="form-label">Sale Date</label>
-    <input type="date" class="form-control" id="sale-date" name="sale_date" value="{{ old('sale_date', $comic->sale_date) }}">
+    <input type="date" class="form-control @error('sale_date') is-invalid @enderror" id="sale-date" name="sale_date" value="{{ old('sale_date', $comic->sale_date) }}">
+    @error('sale_date')<div class="invalid-feedback">{{ $message }}</div>@enderror
   </div>
   <div class="mb-3">
     <label for="type" class="form-label">Type</label>
-    <input type="text" class="form-control" id="type" name="type" value="{{ old('type', $comic->type) }}">
+    <input type="text" class="form-control @error('type') is-invalid @enderror" id="type" name="type" value="{{ old('type', $comic->type) }}">
+    @error('type')<div class="invalid-feedback">{{ $message }}</div>@enderror
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
