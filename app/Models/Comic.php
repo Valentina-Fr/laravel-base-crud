19e1eb5c3808117747d25f12e08d8b8b44de079f
Valentina-Fr <v_frasca@virgilio.it>
2021-10-15 17:32:41 +0200

    Add creation and update date

--- app/Models/Comic.php
+++ app/Models/Comic.php
@@ -4,9 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Carbon\Carbon;
 
 class Comic extends Model
 {
     use SoftDeletes;
     protected $fillable = ['title', 'description', 'thumb', 'price', 'series', 'sale_date', 'type'];
+
+    public function getCreatedAt()
+    {
+        return Carbon::create($this->created_at)->format('d-m-Y');
+    }
+
+    public function getUpdatedAt()
+    {
+        return Carbon::create($this->updated_at)->format('d-m-Y');
+    }
 }
--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -55,6 +55,14 @@
                     <div class="col-5"><h6>Type:</h6></div>
                     <div class="col-7 small-text">{{ $comic->type }}</div>
                 </div>
+                <div class="row align-items-center py-3">
+                    <div class="col-5"><h6>Created at:</h6></div>
+                    <div class="col-7 small-text">{{ $comic->getCreatedAt() }}</div>
+                </div>
+                <div class="row align-items-center py-3">
+                    <div class="col-5"><h6>Updated at:</h6></div>
+                    <div class="col-7 small-text">{{ $comic->getUpdatedAt() }}</div>
+                </div>
             </div>
         </div>
         <div class="container d-flex justify-content-center pb-4">
